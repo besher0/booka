bae08d1faa67c55e40df0473aa67958fe1a0f8b5
besher <bsger@laggan.com>
2025-07-24 05:22:09 +0300

    add

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const memoryUsage = process.memoryUsage();
+function formatBytes(bytes) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
+console.log('استهلاك الذاكرة لعملية Node.js:');
+console.log(`  RSS (Resident Set Size): ${formatBytes(memoryUsage.rss)}`);
     return 'Hello in Booka!';
+
   }
 }
